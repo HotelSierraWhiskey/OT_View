26cba90b807c0ea6507587ba57e0f5b72b327b62
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-02-17 11:58:30 -0500

    Scraper quits webdriver on close

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from qtstrap import *
+from scraper import *
 from calendar_widget import CalendarWidget
+
+
+class MainWindow(BaseMainWindow):
+
+    close_signal = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+    def closeEvent(self, event):
+        self.close_signal.emit()
+        return super().closeEvent(event)
 
 
 def run():
     app = BaseApplication()
-    window = BaseMainWindow()
+
+    scraper = Scraper(SFE_URL)
+
+    calendar = CalendarWidget(scraper)
+
+    window = MainWindow()
+    window.close_signal.connect(scraper.close)
 
     set_font_options(window, {'setPointSize': 12})
     window.setMinimumSize(400, 300)
-
-    calendar = CalendarWidget()
 
     with CVBoxLayout(window) as layout:
         with layout.hbox(align='center') as layout:
@@ -18,6 +35,6 @@ def run():
     window.show()
     app.exec_()
 
- 
+
 if __name__ == "__main__":
     run()
